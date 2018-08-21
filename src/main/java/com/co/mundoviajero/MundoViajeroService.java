85712dfd6f4faf1c2b9af8989b3f6b0ccf7e9024
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2018-08-20 21:47:45 -0500

    Cambios en la estructura del proyecto

--- src/main/java/com/co/mundoviajero/MundoViajeroService.java
+++ src/main/java/com/co/mundoviajero/MundoViajeroService.java
@@ -3,13 +3,25 @@ package com.co.mundoviajero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+//@SpringBootApplication
+//@ComponentScan(basePackages = { "com.co.mundoviajero", "com.co.mundoviajero.controller" })
+
+@Controller
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.co.mundoviajero", "com.co.mundoviajero.controller" })
 public class MundoViajeroService {
-	
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
-        SpringApplication.run(MundoViajeroService.class, args);
-    }
+		SpringApplication.run(MundoViajeroService.class, args);
+	}
 
 }

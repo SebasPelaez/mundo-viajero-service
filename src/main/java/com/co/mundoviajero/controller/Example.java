58c41e8ac6758581c51f4e8756d394f6f51754ac
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2018-08-22 21:41:20 -0500

    Descubrimiento de rutas

--- src/main/java/com/co/mundoviajero/controller/Example.java
+++ src/main/java/com/co/mundoviajero/controller/Example.java
@@ -2,9 +2,9 @@ package com.co.mundoviajero.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/example")
 public interface Example {
 	
-	@RequestMapping("/example")
 	public String home();
 	
 }

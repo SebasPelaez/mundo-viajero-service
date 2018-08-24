19a158df11c7fb2b9cc214261f877bd072942525
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2018-08-23 23:24:07 -0500

    @Service Annotation

--- src/main/java/com/co/mundoviajero/business/StateBusiness.java
+++ src/main/java/com/co/mundoviajero/business/StateBusiness.java
@@ -3,10 +3,12 @@ package com.co.mundoviajero.business;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.co.mundoviajero.persistence.dao.IStateDAO;
 import com.co.mundoviajero.persistence.entity.State;
 
+@Service
 public class StateBusiness {
 
 	@Autowired

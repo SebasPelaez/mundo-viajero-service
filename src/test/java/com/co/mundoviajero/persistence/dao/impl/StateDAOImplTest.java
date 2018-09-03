bb8681fde327f46e57a05a1ea5a918964dac9fd9
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2018-09-02 21:47:54 -0500

    Comienzo de pruebas

---
+++ src/test/java/com/co/mundoviajero/persistence/dao/impl/StateDAOImplTest.java
@@ -0,0 +1,24 @@
+package com.co.mundoviajero.persistence.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StateDAOImplTest {
+
+	@Test
+	public void testGetAllStates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentSession() {
+		fail("Not yet implemented");
+	}
+
+}

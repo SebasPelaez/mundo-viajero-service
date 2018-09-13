7bd63044430ddfe4c8ffeda2e471e4e6548c0665
Juan Pablo Tobón Flórez <juanpbltbn@gmail.com>
2018-09-12 22:58:09 -0500

    Avance con SecurityHandlerInterceptor, busqueda de errores

--- src/main/java/com/co/mundoviajero/business/Login/TokenBusiness.java
+++ src/main/java/com/co/mundoviajero/business/Login/TokenBusiness.java
@@ -4,9 +4,8 @@ import java.security.Key;
 import java.util.Calendar;
 import java.util.Date;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtException;
+import com.co.mundoviajero.util.Constants;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -15,35 +14,20 @@ public class TokenBusiness {
 
 	private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	
-    public static String generateToken(String user) {
+    @SuppressWarnings("deprecation")
+	public static String generateToken(String user) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
-        calendar.add(Calendar.HOUR, 1);
-        /*public static final String CLAVE_JWT = "62703757a09d19c112e19501f77a6faa008ef7e6b193d14afb136f4f8e2fc537244c62c8b5b8bcd64018d373e7160d785cf68e5b8cd9ea6e14f5a31671173189";
-            
-        */
+        calendar.add(Calendar.MINUTE, 1);
         
         Long time = System.currentTimeMillis();
         String jwt = Jwts.builder()
-                .signWith(key)
+                .signWith(SignatureAlgorithm.HS256, Constants.JWT_Key)
                 .setSubject("Juan")
                 .setExpiration(calendar.getTime())
                 .setIssuedAt(new Date(time))
                 .claim("email", user)
                 .compact();        
         return jwt;
-    }
-    
-    public static void processToken(String token){
-    	String email;
-    	try {
-    		Claims claims = Jwts.parser()         
-    			       .setSigningKey(key)
-    			       .parseClaimsJws(token).getBody();     	    
-    	       	     
-    	} catch (JwtException ex) {       
-    	    // Generate exception
-    	    
-    	}
     }
 }
--- src/main/java/com/co/mundoviajero/util/configuration/SecurityHandlerInterceptor.java
+++ src/main/java/com/co/mundoviajero/util/configuration/SecurityHandlerInterceptor.java
@@ -1,7 +1,6 @@
 package com.co.mundoviajero.util.configuration;
 
 import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
@@ -12,65 +11,75 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.com.need.exception.ExpiredTokenException;
-import co.com.need.exception.InvalidTokenException;
-import co.com.need.util.Constantes;
+import com.co.mundoviajero.util.exception.InvalidTokenException;
+import com.co.mundoviajero.util.Constants;
+import com.co.mundoviajero.util.exception.ExpiredTokenException;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 
 public class SecurityHandlerInterceptor implements HandlerInterceptor {
 
-    private MessageSourceAccessor messageSource;
+	private MessageSourceAccessor messageSource;
 
-    public SecurityHandlerInterceptor() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        this.messageSource = new MessageSourceAccessor(messageSource, Locale.ENGLISH);
-    }
+	public SecurityHandlerInterceptor() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		this.messageSource = new MessageSourceAccessor(messageSource, Locale.ENGLISH);
+	}
 
-    @Override
-    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-        throws Exception {
-        /* no-code */
-    }
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		/* no-code */
+	}
 
-    @Override
-    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-        throws Exception {
-        /* no-code */
-    }
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		/* no-code */
+	}
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg) throws Exception {
-        String token = request.getHeader("Authorization");
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg) throws Exception {
+		String token = request.getHeader("Authorization");
 
-        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
-            return true;
-        }
+		if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
+			return true;
+		}
+		System.out.println("El token recibido es: " + token);
+		if (token != null) {
+			
+			try {
+				String[] authParts = token.split("\\s+");
+				System.out.println("Bandera 1: " + authParts.toString());
+				String authInfo = authParts[1];
+				System.out.println("Bandera 2: " + authInfo.toString());
+				/*
+				 * Claims claims =
+				 * Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(Constants.
+				 * JWT_Key)) .parseClaimsJws(authInfo).getBody(); claims.getId();
+				 * claims.getSubject(); claims.getIssuer(); claims.getExpiration();
+				 */
 
-        if (token != null) {
-            try {
-                String[] authParts = token.split("\\s+");
-                String authInfo = authParts[1];
-                //public static final String CLAVE_JWT = "62703757a09d19c112e19501f77a6faa008ef7e6b193d14afb136f4f8e2fc537244c62c8b5b8bcd64018d373e7160d785cf68e5b8cd9ea6e14f5a31671173189";
-                Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(Constantes.CLAVE_JWT))
-                    .parseClaimsJws(authInfo).getBody();
-                claims.getId();
-                claims.getSubject();
-                claims.getIssuer();
-                claims.getExpiration();
-            } catch (ExpiredJwtException e) {
-                throw new ExpiredTokenException(messageSource.getMessage("DESC_EXPIRED_TOKEN"));
-            } catch (Exception e) {
-                throw new InvalidTokenException(messageSource.getMessage("DESC_INVALID_TOKEN"));
-            }
+				Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(Constants.JWT_Key))
+						.parseClaimsJws(authInfo).getBody();
+				System.out.println("Primer claim");
+				claims.getSubject();
+				System.out.println("Segundo claim claim");
+				claims.getExpiration();
+			} catch (ExpiredJwtException e) {
+				throw new ExpiredTokenException(messageSource.getMessage("DESC_EXPIRED_TOKEN"));
+			} catch (Exception e) {
+				throw new InvalidTokenException(messageSource.getMessage("DESC_INVALID_TOKEN"));
+			}
 
-            return true;
-        } else {
-            throw new InvalidTokenException(messageSource.getMessage("DESC_INVALID_TOKEN"));
-        }
-    }
+			return true;
+		} else {
+			throw new InvalidTokenException(messageSource.getMessage("DESC_INVALID_TOKEN"));
+		}
+	}
 
 }

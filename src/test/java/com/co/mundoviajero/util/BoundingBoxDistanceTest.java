7e6113c34f682bc9f5b6c128a32b3e4e7d2a4ac7
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2018-09-29 23:39:02 -0500

    Solve Build Issue

--- src/test/java/com/co/mundoviajero/util/BoundingBoxDistanceTest.java
+++ src/test/java/com/co/mundoviajero/util/BoundingBoxDistanceTest.java
@@ -1,18 +1,12 @@
 package com.co.mundoviajero.util;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import com.co.mundoviajero.util.BoundingBoxDistance;
+import junit.framework.TestCase;
 import com.co.mundoviajero.util.BoundingBoxDistance.BoundingBox;
 import com.co.mundoviajero.util.BoundingBoxDistance.MapPoint;
 
-public class BoundingBoxDistanceTest {
+public class BoundingBoxDistanceTest extends TestCase {
 
-	@Test
 	public void testGetBoundingBox() {
-		
 		MapPoint mp1 = new MapPoint(6.2678539, -75.5690221);
 		MapPoint mpCompare = new MapPoint(6.18461, -75.59913);
 		double halfSideInKm = 20;
@@ -23,7 +17,6 @@ public class BoundingBoxDistanceTest {
 		assertTrue(mpCompare.getLatitude() <= boundingBox.getMaxPoint().getLatitude());
 		assertTrue(mpCompare.getLongitude() >= boundingBox.getMinPoint().getLongitude());
 		assertTrue(mpCompare.getLongitude() <= boundingBox.getMaxPoint().getLongitude());
-				
 	}
 
 }
